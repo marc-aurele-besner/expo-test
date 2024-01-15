9fb797f968f21dae418981c9bcc204c8c5647893
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-01-15 07:57:42 -0500

    add simple test with Counter contract

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
+import { useCallback, useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import "@ethersproject/shims"
+import { Contract, Wallet, providers } from 'ethers'
+import * as Counter from './assets/Counter.json'
 
 export default function App() {
-
-  const test = async () => {
+  const testFetch = async () => {
     console.log('test')
     fetch("https://api.coingecko.com/api/v3/coins/bitcoin", {
       method: "GET",
@@ -22,11 +25,55 @@ export default function App() {
       console.error('Fetch Error:', error);
     });
   }
+  
+  const provider = new providers.JsonRpcProvider(process.env.EXPO_PUBLIC_RPC_URL)
+  const signer = new Wallet(process.env.EXPO_PUBLIC_PRIVATE_KEY, provider)
+  const counter = new Contract(process.env.EXPO_PUBLIC_COUNTER_CONTRACT_ADDRESS, Counter.abi, signer)
+
+  const [currentNumber, setCurrentNumber] = useState('')
+  const [lastTxHash, setLastTxHash] = useState('')
+
+  const getCounter = useCallback(async () => {
+    try {
+      const number = await counter.number()
+      setCurrentNumber(number.toString())
+      console.log('number', number)
+    } catch (error) {
+      console.error('error', error)
+    }
+  }, [counter, setCurrentNumber])
+
+  const setCounter = useCallback(async () => {
+    try {
+      setLastTxHash('sending...')
+      const tx = await counter.setNumber(process.env.EXPO_PUBLIC_NUMBER_TO_SET)
+      setLastTxHash(tx.hash)
+      console.log('tx', tx)
+    } catch (error) {
+      console.error('error', error)
+    }
+  }, [counter, setLastTxHash])
+
+  const incrementCounter = useCallback(async () => {
+    try {
+      setLastTxHash('sending...')
+      const tx = await counter.increment()
+      setLastTxHash(tx.hash)
+      console.log('tx', tx)
+    } catch (error) {
+      console.error('error', error)
+    }
+  }, [counter, setLastTxHash])
 
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Button title="Test" text= "Hello" onPress={test} />
+      <Button title="Test Fetch" text= "Test Fetch" onPress={testFetch} />
+      <Button title="Set Counter" text= "Set Counter" onPress={setCounter} />
+      <Button title="Get Counter" text= "Get Counter" onPress={getCounter} />
+      <Button title="Increment Counter" text= "Increment Counter" onPress={incrementCounter} />
+      <Text>Current Number: {currentNumber}</Text>
+      <Text>Last Tx Hash: {lastTxHash}</Text>
       <StatusBar style="auto" />
     </View>
   );
